93cbd7f4d2757da51ab11e949278bc3e7869b937
unknown <oficinadosdrones@gmail.com>
2022-03-08 17:50:31 +0000

    criando diretorios para oganizar o codigo

--- scripts/controler/buttonsControler.js
+++ scripts/controler/buttonsControler.js
@@ -1,18 +1,44 @@
 /**Classe dos botoes da calculator */
 class ButtonsControler {
   _buttons;
+  _eventsMouse;
+  _eventCursorMouse;
   /**Pegando os TODOS Eventos do botão */
+
+  constructor() {
+    this._eventsMouse = "click drag";
+    this._eventCursorMouse = "mouseover mouseup mousedown";
+  }
+
   initButtonEvents() {
     /**Usando o QUERY_SELECTOR_ALL , pegando um, array de buttons e suas TAGs filhas > */
     this.buttons = document.querySelectorAll("#buttons > g, #parts > g");
-    this.buttons.forEach((btn, _index) => {
-      btn.addEventListener("click", (env) => {
+    this.buttons.forEach((btn) => {
+      this.addEventListenerALL(btn, this._eventsMouse, () => {
         console.log(btn.className.baseVal.replace("btn-", ""));
-        return btn.className.baseVal.replace("btn-", "");
       });
+    });
+    /**mudando o cursor do mouse */
+    this.buttons.forEach((btn) => {
+      this.changeMouseCursor(btn, this._eventCursorMouse, () => {});
     });
   }
 
+  /**Criando um Função local EventListenerALL, pois all não existem no JS */
+  addEventListenerALL(btn, eventNames, callback) {
+    eventNames.split(" ").forEach((eventName) => {
+      btn.addEventListener(eventName, callback, false);
+    });
+  }
+  /**Criando uma função que muda o cursor point do mouse na medida que passamos na tecla do calculator */
+  changeMouseCursor(btn, eventNames, callback) {
+    eventNames.split(" ").forEach((eventName) => {
+      btn.style.cursor = "pointer";
+      btn.addEventListener(eventName, callback, false);
+    });
+  }
+
+  /**GETS e SETS */
   get buttons() {
     return this._buttons;
   }
